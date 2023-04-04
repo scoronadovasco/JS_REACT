396a40e38dcb456fe64b3f8471018f5c1f3a21e7
Santiago C <scoronado@integrupo.com>
2023-04-04 17:32:39 -0500

    algunos temas a repasar

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,3 +1,1 @@
-const saludo = "hola";
-
-print(saludo)
+print("hola")
---
+++ Javascript/variables.js
@@ -0,0 +1,13 @@
+var nombre= "santaiago";
+
+var edad;
+
+edad = 33;
+
+edad
+
+var persona = {
+    nombre: "santiago",
+    edad: 22,
+    lvivienda : "medellin"
+}
