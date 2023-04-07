fab6ef533286f37d834c61217d66a65197d712ae
Santiago C <scoronado@integrupo.com>
2023-04-07 16:00:08 -0500

    queryselector en js

---
+++ script.js
@@ -0,0 +1,12 @@
+var h1 = document.querySelector('h1')
+let pa = document.querySelector('#pid')
+let parrafo = document.querySelector('.parrafo')
+var inp = document.querySelector('input')
+
+console.log({
+    h1,
+    pa,
+    parrafo,
+    inp
+
+});
