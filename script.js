796b0ab5138c9f1f2e77a267e2a6cf33636aa4b6
Santiago C <scoronado@integrupo.com>
2023-04-07 18:13:46 -0500

    concepts of js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 var h1 = document.querySelector('h1')
-let pa = document.querySelector('#pid')
-let parrafo = document.querySelector('.parrafo')
-var inp = document.querySelector('input')
+let formu = document.getElementById('formulario1')
+var result = document.querySelector('#result') 
+let input1 = document.querySelector('#calculo1')
+let input2 = document.querySelector('#calculo2')
+let Btncal = document.querySelector('#btncalcular ')
 
-console.log({
-    h1,
-    pa,
-    parrafo,
-    inp
 
-});
+function summarinpuntValues(event){
+    event.preventDefault();
+    let sumainp =parseInt(input1.value) + parseInt( input2.value);
+    result.innerText= 'Resultado :' + sumainp;
+
+}
+
+formu.addEventListener('submit',summarinpuntValues)
