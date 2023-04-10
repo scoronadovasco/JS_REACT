ff908cc60bfbd53c41a058cd399075b62a9647f1
Santiagocrocs <scoronadovasco@gmail.com>
2023-04-09 20:53:30 -0500

    add all files

---
+++ Javascript/basesJS/bases.js
@@ -0,0 +1,95 @@
+var arraysecreto = []; 
+
+function solution(arraysecreto) {
+    if(arraysecreto[1] == typeof(String)){
+        console.log(true)
+    }else{false}
+  }
+
+
+  var  frutas = ['manzana', 'banana', 'naranja', 'kiwi', 'uva'];
+
+for(var i=0; i< frutas.length; i++){
+
+    console.log(frutas[i])
+    
+}i++;
+
+const array1 = ['a', 'b', 'c'];
+
+array1.forEach(element => console.log(element));
+
+
+var numero1 = 33;
+var numero2 = 21;
+
+console.log(numero1 === numero2)
+
+
+if(4=3){
+    return true
+}else{}
+
+
+var num = 32;
+undefined
+var num2 = 23;
+undefined
+if (num == num2) {
+    console.log(true)
+}else{console.log(false)}
+undefined
+if (num == num2) {
+    console.log(true)
+}else{console.log(false)}
+ false
+undefined
+if (num == num2) {
+    console.log(true)
+}else if (num2 == 23) 
+    
+{console.log("correcto")}
+
+
+
+
+var caso = 2;
+
+switch(caso){
+    case 1 :
+        console.log("primer")
+        break;
+
+    case 2: 
+    console.log("segundo")
+}
+
+
+function solution(article) {
+    switch (article) {
+  
+      case "computadora":
+  
+        console.log("Con mi computadora puedo programar usando JavaScript")
+        break;
+  
+      case "celular":
+  
+        console.log("En mi celular puedo aprender usando la app de Platzi")
+        break;
+  
+      case "cable":
+  
+        console.log("¡Hay un cable en mi bota!")
+        break;
+  
+      default:
+        console.log("Artículo no encontrado")
+        break;
+    }
+  }
+  
+  solution('computadora')
+  solution('celular')
+  solution('cable')
+  solution('ornitorrinco')
